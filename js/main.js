e033712c9e4d80f1f49c6f0e230b5137ac288816
George0112 <youmaker12345@gmail.com>
2017-05-01 22:09:32 +0800

    update deadline and video

--- js/main.js
+++ js/main.js
@@ -1,68 +1,69 @@
-jQuery(function($) {'use strict',
+jQuery(function($) {
+    'use strict',
 
-	//Countdown js
-	 $("#countdown").countdown({
-			date: "9 may 2017 12:00:00",
-			format: "on"
-		},
+    //Countdown js
+    $("#countdown").countdown({
+            date: "9 may 2017 23:59:59",
+            format: "on"
+        },
 
-		function() {
-			// callback function
-		});
-
-
-
-	//Scroll Menu
-
-	function menuToggle()
-	{
-		var windowWidth = $(window).width();
-
-		if(windowWidth > 767 ){
-			$(window).on('scroll', function(){
-				if( $(window).scrollTop()>405 ){
-					$('.main-nav').addClass('fixed-menu animated slideInDown');
-				} else {
-					$('.main-nav').removeClass('fixed-menu animated slideInDown');
-				}
-			});
-		}else{
-
-			$('.main-nav').addClass('fixed-menu animated slideInDown');
-
-		}
-	}
-
-	menuToggle();
+        function() {
+            // callback function
+        });
 
 
-	// Carousel Auto Slide Off
-	$('#event-carousel, #twitter-feed, #process-carousel ').carousel({
-		interval: false
-	});
+
+    //Scroll Menu
+
+    function menuToggle() {
+        var windowWidth = $(window).width();
+
+        if (windowWidth > 767) {
+            $(window).on('scroll', function() {
+                if ($(window).scrollTop() > 405) {
+                    $('.main-nav').addClass('fixed-menu animated slideInDown');
+                } else {
+                    $('.main-nav').removeClass('fixed-menu animated slideInDown');
+                }
+            });
+        } else {
+
+            $('.main-nav').addClass('fixed-menu animated slideInDown');
+
+        }
+    }
+
+    menuToggle();
 
 
-	// Contact form validation
-	var form = $('.contact-form');
-	form.submit(function () {'use strict',
-		$this = $(this);
-		$.post($(this).attr('action'), function(data) {
-			$this.prev().text(data.message).fadeIn().delay(3000).fadeOut();
-		},'json');
-		return false;
-	});
+    // Carousel Auto Slide Off
+    $('#event-carousel, #twitter-feed, #process-carousel ').carousel({
+        interval: false
+    });
 
-	$( window ).resize(function() {
-		menuToggle();
-	});
 
-	$('.main-nav ul').onePageNav({
-		currentClass: 'active',
-	    changeHash: false,
-	    scrollSpeed: 900,
-	    scrollOffset: 0,
-	    scrollThreshold: 0.3,
-	    filter: ':not(.no-scroll)'
-	});
+    // Contact form validation
+    var form = $('.contact-form');
+    form.submit(function() {
+        'use strict',
+        $this = $(this);
+        $.post($(this).attr('action'), function(data) {
+            $this.prev().text(data.message).fadeIn().delay(3000).fadeOut();
+        }, 'json');
+        return false;
+    });
+
+    $(window).resize(function() {
+        menuToggle();
+    });
+
+    $('.main-nav ul').onePageNav({
+        currentClass: 'active',
+        changeHash: false,
+        scrollSpeed: 900,
+        scrollOffset: 0,
+        scrollThreshold: 0.3,
+        filter: ':not(.no-scroll)'
+    });
 
 });
